0706311d00fe1b884a3aae3ced490565ead77d08
Achyut Neupane <30431426+achyutkneupane@users.noreply.github.com>
2024-04-22 21:29:57 +0545

    feat: Tailwind installed (#2)
    * Tailwind installed
    
    * Boilerplate cleaned
    
    * Pakcage lock removed

---
+++ src/routes/Sidebar.tsx
@@ -0,0 +1,13 @@
+import {component$} from "@builder.io/qwik";
+
+const Sidebar = component$(() => {
+  return (
+      <>
+        <h1 class="text-3xl font-bold underline">
+          Sidebar
+        </h1>
+      </>
+  );
+});
+
+export default Sidebar;
