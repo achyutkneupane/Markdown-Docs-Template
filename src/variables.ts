25a8e335251571038afbb528200f3c0436a3ed89
Achyut Neupane <achyutkneupane@gmail.com>
2024-04-22 23:07:49 +0545

    Variables from .env

---
+++ src/variables.ts
@@ -0,0 +1,5 @@
+export const appName : string = import.meta.env.VITE_APP_NAME;
+
+export const appVersion : string = import.meta.env.VITE_APP_VERSION;
+
+export const githubUrl : string = import.meta.env.VITE_GITHUB_URL;
