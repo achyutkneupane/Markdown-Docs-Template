dc7ba37c94963f9b7b1c1b471f4789dc4d3321aa
Achyut Neupane <30431426+achyutkneupane@users.noreply.github.com>
2024-04-22 22:42:58 +0545

    feat: Page layout designed with static content (#3)
    * Page separated to sidebar and slot
    
    * Navbar added as fixed top
    
    * Loop inside the sidebar items
    
    * Sidebar style created
    
    * Github icon in navbar

--- src/routes/Sidebar.tsx
+++ src/routes/Sidebar.tsx
@@ -1,13 +1,34 @@
-import {component$} from "@builder.io/qwik";
+import {component$, Fragment} from "@builder.io/qwik";
+import {sidebarItems} from "~/routes/sidebarGenerator";
+import type {SidebarItemProps} from "~/types";
+
+const items: SidebarItemProps[] = sidebarItems;
+
+const currentRoute = '/';
 
 const Sidebar = component$(() => {
-  return (
-      <>
-        <h1 class="text-3xl font-bold underline">
-          Sidebar
-        </h1>
-      </>
-  );
+    return (
+        <div class="px-4 py-2 flex flex-col gap-2 bg-gray-50 h-screen">
+            {items.map((item: SidebarItemProps, index: number) => (
+                <Fragment key={`sidebarItem-${index}`}>
+                    <div
+                        class={`${item.subItems ? 'sidebar-item-with-subitems' : 'sidebar-item'} ${currentRoute === item.href ? 'active' : ''}`}>
+                        {item.label}
+                    </div>
+                    {item.subItems && (
+                        <div class="ml-4 flex flex-col gap-1">
+                            {item.subItems.map((subItem: SidebarItemProps, subIndex: number) => (
+                                <div key={`sidebarSubItem-${subIndex}`}
+                                     class={`sidebar-sub-item  ${currentRoute === subItem.href ? 'active' : ''}`}>
+                                    {subItem.label}
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </Fragment>
+            ))}
+        </div>
+    );
 });
 
 export default Sidebar;
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,23 +1,33 @@
-import { component$, Slot } from "@builder.io/qwik";
-import type { RequestHandler } from "@builder.io/qwik-city";
+import {component$, Slot} from "@builder.io/qwik";
+import type {RequestHandler} from "@builder.io/qwik-city";
 import Sidebar from "~/routes/Sidebar";
+import Navbar from "~/routes/Navbar";
 
-export const onGet: RequestHandler = async ({ cacheControl }) => {
-  // Control caching for this request for best performance and to reduce hosting costs:
-  // https://qwik.dev/docs/caching/
-  cacheControl({
-    // Always serve a cached response by default, up to a week stale
-    staleWhileRevalidate: 60 * 60 * 24 * 7,
-    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-    maxAge: 5,
-  });
+export const onGet: RequestHandler = async ({cacheControl}) => {
+    // Control caching for this request for best performance and to reduce hosting costs:
+    // https://qwik.dev/docs/caching/
+    cacheControl({
+        // Always serve a cached response by default, up to a week stale
+        staleWhileRevalidate: 60 * 60 * 24 * 7,
+        // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+        maxAge: 5,
+    });
 };
 
 export default component$(() => {
-  return (
-      <>
-        <Sidebar />
-        <Slot />
-      </>
-  );
+    return (
+        <>
+            <Navbar />
+            <div class="px-56">
+                <div class="flex justify-start align-middle pt-20 divide-x">
+                    <div class="w-96 h-screen fixed">
+                        <Sidebar/>
+                    </div>
+                    <div class="ml-96 w-full p-2 bg-gray-50">
+                        <Slot/>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 });
