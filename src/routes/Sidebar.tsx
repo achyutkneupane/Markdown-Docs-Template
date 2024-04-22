4d26f2946370958bf426fc7a90c7eb7e22e4de3d
Achyut Neupane <achyutkneupane@gmail.com>
2024-04-22 22:24:54 +0545

    Sidebar style created

--- src/routes/Sidebar.tsx
+++ src/routes/Sidebar.tsx
@@ -4,18 +4,20 @@ import type {SidebarItemProps} from "~/types";
 
 const items: SidebarItemProps[] = sidebarItems;
 
+const currentRoute = '/';
+
 const Sidebar = component$(() => {
     return (
-        <div class="px-4 py-2 flex flex-col gap-2">
+        <div class="px-4 py-2 flex flex-col gap-2 bg-gray-50 h-screen">
             {items.map((item: SidebarItemProps, index: number) => (
                 <Fragment key={`sidebarItem-${index}`}>
-                    <div class="sidebar-item">
+                    <div class={`${item.subItems ? 'sidebar-item-with-subitems' : 'sidebar-item'} ${currentRoute === item.href ? 'active' : ''}`}>
                         {item.label}
                     </div>
                     {item.subItems && (
-                        <div class="ml-4">
+                        <div class="ml-4 flex flex-col gap-1">
                             {item.subItems.map((subItem: SidebarItemProps, subIndex: number) => (
-                                <div class="sidebar-sub-item" key={`sidebarSubItem-${subIndex}`}>
+                                <div key={`sidebarSubItem-${subIndex}`} class={`sidebar-sub-item  ${currentRoute === subItem.href ? 'active' : ''}`}>
                                     {subItem.label}
                                 </div>
                             ))}
