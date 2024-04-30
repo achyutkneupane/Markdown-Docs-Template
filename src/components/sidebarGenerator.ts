2c8da483fffc1eaf16f02ea2362e60b9904cca5a
Achyut Neupane <achyutkneupane@gmail.com>
2024-05-01 01:52:29 +0545

    Nested route url

--- src/components/sidebarGenerator.ts
+++ src/components/sidebarGenerator.ts
@@ -10,25 +10,26 @@ const file = fs.readFileSync(fullPath, 'utf8');
 
 const parsedYML = parse(file).routes;
 
-function extractRoutes(obj : any): SidebarItemProps[] {
+function extractRoutes(prevKey: string, obj : any): SidebarItemProps[] {
     const routes: SidebarItemProps[] = [];
 
     obj?.forEach((item: any) => {
         if (typeof item === 'object') {
             for (const [key, values] of Object.entries(item)) {
-                routes.push({ label: labelParser(key), subItems: extractRoutes(values) });
+                routes.push({ label: labelParser(key), subItems: extractRoutes(key, values) });
             }
         } else {
             if(item === 'home') {
-                routes.push({ label: 'Home', href: '/' });
+                console.log(prevKey);
+                routes.push({ label: 'Home', href: `/${prevKey}` });
                 return;
             }
-            routes.push({ label: labelParser(item), href: `/${item}` });
+            routes.push({ label: labelParser(item), href: `${prevKey ? `/${prevKey}` : ''}/${item}` });
         }
     });
     return routes;
 }
 
-const sidebarItems: SidebarItemProps[] = extractRoutes(parsedYML);
+const sidebarItems: SidebarItemProps[] = extractRoutes("",parsedYML);
 
 export default sidebarItems;
