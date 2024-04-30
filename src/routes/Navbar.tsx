8b92921dd20d876af3f0264a83c2c157f4179830
Achyut Neupane <achyutkneupane@gmail.com>
2024-04-30 21:38:47 +0545

    README updated

--- src/routes/Navbar.tsx
+++ src/routes/Navbar.tsx
@@ -1,5 +1,5 @@
 import {component$} from "@builder.io/qwik";
-import github from "../../public/github.svg";
+import github from "/github.svg";
 import {appName, appVersion, githubUrl} from "~/variables";
 
 const Navbar = component$(() => {
