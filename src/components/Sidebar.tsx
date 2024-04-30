b66276158328554d46a9b45dd1c95e497f0fffbf
Achyut Neupane <30431426+achyutkneupane@users.noreply.github.com>
2024-05-01 02:19:50 +0545

    feat: Parses route from YAML file (#6)
    * Routes parsed from routes folder
    
    * Routes computed for re-render prevention
    
    * Sidebar generated from YAML
    
    * With slash to set active route
    
    * Nested route url
    
    * Removed the undetected route from parsed
    
    * Test route 'achyut' created

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,35 +1,43 @@
-import {component$, Fragment} from "@builder.io/qwik";
-import {sidebarItems} from "~/components/sidebarGenerator";
+import {component$, Fragment, useComputed$} from "@builder.io/qwik";
+import sidebarItems from "~/components/sidebarGenerator";
 import type {SidebarItemProps} from "~/types";
 import {routesAreSame} from "~/components/helpers";
 import {Link, useLocation} from '@builder.io/qwik-city';
-
-const items: SidebarItemProps[] = sidebarItems;
 
 const Sidebar = component$(() => {
 
     const loc = useLocation();
     const currentRoute = loc.url.pathname;
-
+    const items: SidebarItemProps[] = useComputed$(() => sidebarItems).value;
     return (
         <div class="px-4 py-2 flex flex-col gap-2 bg-gray-50 h-screen">
-            {items.map((item: SidebarItemProps, index: number) => (
-                <Fragment key={`sidebarItem-${index}`}>
-                    <Link href={item.href || ""} class={`sidebar-item ${routesAreSame(currentRoute, item.href || "") ? 'active' : ''}`}>
-                        {item.label}
-                    </Link>
-                    {item.subItems && (
-                        <div class="ml-4 flex flex-col gap-1">
-                            {item.subItems.map((subItem: SidebarItemProps, subIndex: number) => (
-                                <Link key={`sidebarSubItem-${subIndex}`} href={subItem.href || ""}
-                                      class={`sidebar-sub-item  ${routesAreSame(currentRoute, subItem.href || "") ? 'active' : ''}`}>
-                                    {subItem.label}
-                                </Link>
-                            ))}
-                        </div>
-                    )}
-                </Fragment>
-            ))}
+            {items.map((item: SidebarItemProps, index: number) => {
+                return (
+                    <Fragment key={`sidebarItem-${index}`}>
+                        {item.subItems ? (
+                            <div
+                                class={`sidebar-item-with-subitems`}>
+                                {item.label}
+                            </div>
+                        ) : (
+                            <Link href={item.href || ""}
+                                  class={`sidebar-item ${routesAreSame(currentRoute, item.href || "") ? 'active' : ''}`}>
+                                {item.label}
+                            </Link>
+                        )}
+                        {item.subItems && (
+                            <div class="ml-4 flex flex-col gap-1">
+                                {item.subItems.map((subItem: SidebarItemProps, subIndex: number) => (
+                                    <Link key={`sidebarSubItem-${subIndex}`} href={subItem.href || ""}
+                                          class={`sidebar-sub-item  ${routesAreSame(currentRoute, subItem.href || "") ? 'active' : ''}`}>
+                                        {subItem.label}
+                                    </Link>
+                                ))}
+                            </div>
+                        )}
+                    </Fragment>
+                );
+            })}
         </div>
     );
 });
--- src/components/helpers.ts
+++ src/components/helpers.ts
@@ -1,4 +1,9 @@
-function routesAreSame(routeOne: string, routeTwo: string) {
+import type {SidebarItemProps} from "~/types";
+
+function routesAreSame(routeOne?: string, routeTwo?: string) {
+    if (!routeOne || !routeTwo) {
+        return false;
+    }
     if (routeOne.endsWith('/')) {
         routeOne = routeOne.slice(0, -1);
     }
@@ -8,4 +13,29 @@ function routesAreSame(routeOne: string, routeTwo: string) {
     return routeOne === routeTwo;
 }
 
-export { routesAreSame };
+function replaceAll(str: string, find: string, replace: string) : string {
+    return str.replace(new RegExp(find, 'g'), replace);
+}
+
+function labelParser(str: string) {
+    const withoutSymbol = replaceAll(str, '-', ' ');
+    return withoutSymbol.replaceAll(/\b\w/g, (char) => char.toUpperCase());
+}
+
+function firstLetterToUpperCase(str: string) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function routeParser(route: string): SidebarItemProps {
+    const noExtension = route.replace('.mdx', '');
+    const fullRouteWithoutRootFolder = noExtension.replace('/index', '/');
+    const label: string = fullRouteWithoutRootFolder.replaceAll('/', '').replace('-', ' ');
+    if (label === '') {
+        return { href: fullRouteWithoutRootFolder, label: 'Home' };
+    } else {
+        const flLabel = firstLetterToUpperCase(label);
+        return { href: fullRouteWithoutRootFolder, label: flLabel };
+    }
+}
+
+export { routesAreSame, labelParser, routeParser };
